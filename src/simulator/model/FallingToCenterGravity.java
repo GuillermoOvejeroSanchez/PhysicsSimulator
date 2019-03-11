e77395c52995f4db74450e2b4b202f1f69f3f87a
GuillermoOvejeroSanchez <guilleov@ucm.es>
2019-03-11 02:57:24 +0100

    Update FTCG

--- src/simulator/model/FallingToCenterGravity.java
+++ src/simulator/model/FallingToCenterGravity.java
@@ -10,26 +10,11 @@ public class FallingToCenterGravity implements GravityLaws{
 	
 	
 	public void apply(List<Body> bodies) {
-		double[] acc = {0.0,g}; 
 		Body body; 
-		
-		Vector gravedad= new Vector(acc); 
-		double[] _center = {0.0,0.0};
-		Vector center = new Vector(_center);
 		for(int i = 0; i < bodies.size(); i++) {
 			body = bodies.get(i);
 			body.setAcceleration(body.getPosition().direction().scale(-g));
 		}
 		
 	}
-	
-	private boolean checkCenter(Body body) {
-
-		if(body.getPosition().coordinate(1) < 0)
-			return true;
-		
-		return false;
-	}
-
-	
 }
